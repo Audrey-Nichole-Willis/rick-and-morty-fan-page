cf1940330a10a22b0ed363f2d18f8e282c064461
Audrey-Nichole-Willis <Audrey.Nichole.Willis@outlook.com>
2022-06-11 14:55:09 -0400

    changed opening page header font size, imported animate.css, and used it to create a light speed entrance for header

--- src/Opening Page/OpeningPage.js
+++ src/Opening Page/OpeningPage.js
@@ -2,11 +2,11 @@ import React from "react";
 import "./OpeningPage.scss";
 
 function OpeningPage(){
-    return(
-        <div className="opening-page-container">
-            <h1 className="opening-page-header">Rick and Morty</h1>
-            <button className="opening-page-button">Start your adventure here!</button>
-        </div>
-    )
+    return (
+      <div className="opening-page-container">
+        <h1 className="opening-page-header animate__animated animate__lightSpeedInLeft"> Rick and Morty</h1>
+        <button className="opening-page-button">Start your adventure here! </button>
+      </div>
+    );
 }
 export default OpeningPage;
