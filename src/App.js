fde3cf52dee67153a85ebbb8c390a86c538126c6
Audrey-Nichole-Willis <Audrey.Nichole.Willis@outlook.com>
2022-06-11 14:34:39 -0400

    set up basic html for opening page and set up a scss file for it

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import OpeningPage from "./Opening Page/OpeningPage";
+
+function App() {
+  return (
+    <div className="App">
+      <OpeningPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Opening Page/OpeningPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./OpeningPage.scss";
+
+function OpeningPage(){
+    return(
+        <div className="opening-page-container">
+            <h1 className="opening-page-header">Rick and Morty</h1>
+            <button className="opening-page-button">Start your adventure here!</button>
+        </div>
+    )
+}
+export default OpeningPage;
