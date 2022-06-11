9f123e8dc9f6f8199a797a69e9460229295523c8
Audrey-Nichole-Willis <Audrey.Nichole.Willis@outlook.com>
2022-06-11 17:39:44 -0400

    added styling to the opening page. including font and background colors as well as a border radius on the button wit flex box to center everything

--- src/Opening Page/OpeningPage.js
+++ src/Opening Page/OpeningPage.js
@@ -4,14 +4,16 @@ import "./OpeningPage.scss";
 function OpeningPage(){
     return (
       <div className="opening-page-container">
-        <div className="opening-page-header-container animate__animated animate__lightSpeedInLeft">
-          <h1 className="opening-page-header animate__animated animate__wobble animate__delay-2s">
-            Rick and Morty
-          </h1>
-        </div>
-        <button className="opening-page-button animate__animated animate__fadeInDown animate__delay-3s">
-          Start your adventure here!
+        <div className="opening page-content">
+          <div className="opening-page-header-container animate__animated animate__lightSpeedInLeft">
+            <h1 className="opening-page-header animate__animated animate__wobble animate__delay-2s">
+              Rick And Morty Fan Page
+            </h1>
+          </div>
+          <button className="opening-page-button animate__animated animate__fadeInDown animate__delay-3s">
+            Start your adventure here!
           </button>
+        </div>
       </div>
     );
 }
