40405d32e5ed729747dc6f60387e4caf8a9d3137
Audrey-Nichole-Willis <Audrey.Nichole.Willis@outlook.com>
2022-06-14 19:50:20 -0400

    added a photo

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Nav from "./Nav/Nav";
 import "./Header.scss";
+import "./rick-and-morty-gd06010ed7_1920.jpg";
 
 function Header(){
     return(
         <div className="header-container">
             <Nav/>
+            <img src = {require("./rick-and-morty-gd06010ed7_1920.jpg")} alt = "rick and morty"/>
             <h1 className="header-title">Rick and Morty</h1>
         </div>
     )
