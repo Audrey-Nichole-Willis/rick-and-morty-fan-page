0b819cb25dfe0770a348e03ec84e875ea97cb86c
Audrey-Nichole-Willis <Audrey.Nichole.Willis@outlook.com>
2022-06-15 15:21:33 -0400

    wrote out the html for the about section of the homepage.

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -5,6 +5,10 @@ function HomePage(){
     return(
         <div className = "home-page-container">
             <Header/>
+            <div className = "homepage-about-section">
+                <h2 className = "homepage-about-title">Welcome Intergalactic  travelers!</h2>
+                <p className = "homepage-about-text">This is a fan page dedicated to one of the most intelligent  and funny adult cartoons on the  market today. Make your own Rick and Morty meme or play some trivia in the games section of the site. Find out which Rick and Morty character you are and more in the Quiz section. You can even check out the lists of episodes and characters to learn more about them. Indulge in your love of Rick and Morty and let you inner fan out to play. </p>
+            </div>
         </div>
     )
 }
