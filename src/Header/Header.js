3df41cff8e7255610daaa110eb7110f1b3ded637
Audrey-Nichole-Willis <Audrey.Nichole.Willis@outlook.com>
2022-06-15 13:05:14 -0400

    removed border from header and made header title longer

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,15 +1,15 @@
 import React from "react";
 import Nav from "./Nav/Nav";
 import "./Header.scss";
-import "./rick-and-morty-gd06010ed7_1920.jpg";
 
 function Header(){
-    return(
-        <div className="header-container">
-            <Nav/>
-            <img src = {require("./rick-and-morty-gd06010ed7_1920.jpg")} alt = "rick and morty"/>
-            <h1 className="header-title">Rick and Morty</h1>
+    return (
+      <div className="header-container">
+        <Nav />
+        <div className="header-img-and-title">
+          <h1 className="header-title">Rick and Morty Fan Page</h1>
         </div>
-    )
+      </div>
+    );
 }
 export default Header;
